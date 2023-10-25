e8dfbfd4abf60c78c41c91883a206b3c073f6d07
Istiak38 <listiakahamad63@gmail.com>
2023-10-25 13:35:32 -0700

    Update home.jsx

--- Car-Doc/src/Pages/Home/Home.jsx
+++ Car-Doc/src/Pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Banner from './Banner';
+import About from './About';
 
 const Home = () => {
     return (
         <div>
+
+            <Banner></Banner>
+            <About></About>
 
             <h1>This is Home</h1>
             
